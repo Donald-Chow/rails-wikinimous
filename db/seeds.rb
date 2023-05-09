00ad9d64ea205d7b0a24b89f22edbe7de81de24d
Donald Chow <donald7290@gmail.com>
2023-05-09 15:16:23 +0900

    Complete Rails Wikinimous Challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times {
+  title = "#{Faker::Beer.brand} - #{Faker::Beer.name}"
+  content = "#{Faker::Beer.style} - Alcohol %: #{Faker::Beer.alcohol}"
+  Article.create(title: title, content: content)
+}
